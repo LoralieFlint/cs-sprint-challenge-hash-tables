738236fef7bd0b48caa92aafca05e1d5de7a7edc
loralie flint <loralie.flint0128@gmail.com>
2020-11-07 12:02:14 -0800

    example one done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,8 +1,18 @@
-# comment to make first commit
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # create dictionary
+    pairs = {}
+        
+    # storing to the dictionary (storing values as keys in pairs)
+    for item in range(length):
+        pairs[weights[item]] = item
 
+    # looping to check if the result of limit - the value of weights exists in pairs
+    for item in range(length):
+        if limit - weights[item] in pairs:
+            # returning the higher valued index first
+            return (max(item, pairs[limit - weights[item]]), min(item, pairs[limit - weights[item]]))
+
+    # if pairs dont exist return none
     return None
+
+   
